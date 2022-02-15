e30e9f47008742912e78d48e8c43227cd235aab5
hwankee <abcy100@nate.com>
2022-02-15 21:32:05 +0900

    add openGL Tutorials
    http://www.opengl-tutorial.org/kr/beginners-tutorials/

---
+++ GRAPHICS API/openGL/cmake-build-debug/CMakeFiles/launch-tutorial08_basic_shading.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+bindir=$(pwd)
+cd /Users/BrightLand/Learn_MetalAPI/GRAPHICS API/openGL/tutorial08_basic_shading/
+export 
+
+if test "x$1" = "x--debugger"; then
+	shift
+	if test "x" = "xYES"; then
+		echo "r  " > $bindir/gdbscript
+		echo "bt" >> $bindir/gdbscript
+		GDB_COMMAND-NOTFOUND -batch -command=$bindir/gdbscript  /Users/BrightLand/Learn_MetalAPI/GRAPHICS\ API/openGL/cmake-build-debug/tutorial08_basic_shading 
+	else
+		"/Users/BrightLand/Learn_MetalAPI/GRAPHICS\ API/openGL/cmake-build-debug/tutorial08_basic_shading"  
+	fi
+else
+	"/Users/BrightLand/Learn_MetalAPI/GRAPHICS\ API/openGL/cmake-build-debug/tutorial08_basic_shading"  
+fi
